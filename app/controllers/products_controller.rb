ee74bcddd82471dcb14034e0ee703f4c88873138
AronLowe <lowe.aron@gmail.com>
2017-03-12 11:16:53 -0500

    wit ability to edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,5 +19,28 @@ class ProductsController < ApplicationController
     render "listings_create.html.erb"
   end
 
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "listings_show.html.erb"
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "listings_edit.html.erb"
+  end
+
+  def update
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.name = params[:form_name]
+    @product.price = params[:form_price]
+    @product.image = params[:form_image]
+    @product.description = params[:form_description]
+    @product.save
+    render "listings_update.html.erb"
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get "/listings" => "products#listings_method"
   get "/listings/new" => "products#new"
   post "/listings" => "products#create"
+  get "/listings/:id" => "products#show"
+  #need to acheive this through a link _ need to fixe imaging
+  get "/listings/:id/edit" => "products#edit"
+  patch "/listings/:id" => "products#update"
 end
 
-# name, price, image, description
